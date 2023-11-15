bb7cb1288fd93d3329dc9006a8dbbeaad23ebc66
unknown <kaurrajinder95@gmail.com>
2023-11-14 20:53:36 -0500

    uncommented a Syso

--- src/amusmentPark/Park.java
+++ src/amusmentPark/Park.java
@@ -2,18 +2,17 @@ package amusmentPark;
 
 public class Park {
 	int height;
-	
-	
+
 	void rides() {
-		
-		//System.out.println("Enter the Height");
-		if(height>=90 && height<=200)
+
+		System.out.println("Enter the Height");
+		if (height >= 90 && height <= 200)
 			System.out.println("Allowed to Take Rides");
-	
-	else {
-		System.out.println("Not Allowed");
-	}
-	
-	}
+
+		else {
+			System.out.println("Not Allowed");
+		}
 
 	}
+
+}
