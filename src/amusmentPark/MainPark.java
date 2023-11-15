195972bba6138887c14b361a30e5429cae66a059
unknown <kaurrajinder95@gmail.com>
2023-11-14 20:25:33 -0500

    Amusement park program

---
+++ src/amusmentPark/MainPark.java
@@ -0,0 +1,12 @@
+package amusmentPark;
+
+public class MainPark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Park park=new Park();
+     park.height=90;
+     park.rides();
+	}
+
+}
---
+++ src/amusmentPark/Park.java
@@ -0,0 +1,19 @@
+package amusmentPark;
+
+public class Park {
+	int height;
+	
+	
+	void rides() {
+		
+		//System.out.println("Enter the Height");
+		if(height>=90 && height<=200)
+			System.out.println("Allowed to Take Rides");
+	
+	else {
+		System.out.println("Not Allowed");
+	}
+	
+	}
+
+	}
