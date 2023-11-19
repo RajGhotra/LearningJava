7ef849a3a516e7f40cdc8ac609fd39ae338e398c
unknown <kaurrajinder95@gmail.com>
2023-11-19 16:09:02 -0500

    amusment park assignment

--- src/amusmentPark/MainPark.java
+++ src/amusmentPark/MainPark.java
@@ -1,12 +1,17 @@
 package amusmentPark;
 
+import java.util.Scanner;
+
 public class MainPark {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     Park park=new Park();
-     park.height=90;
-     park.rides();
+		Park park = new Park();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The height");
+		park.height = sc.nextInt();
+		park.rides();
 	}
 
 }
--- src/amusmentPark/Park.java
+++ src/amusmentPark/Park.java
@@ -2,17 +2,25 @@ package amusmentPark;
 
 public class Park {
 	int height;
+	int noOfPerson;
 
 	void rides() {
 
-		System.out.println("Enter the Height");
-		if (height >= 90 && height <= 200)
-			System.out.println("Allowed to Take Rides");
+		// if (height >= 90 && height <= 200)
+		
+		for (noOfPerson = 0; noOfPerson < 11; noOfPerson++) {
 
-		else {
-			System.out.println("Not Allowed");
+			System.out.println("Space for peole to ride:" + noOfPerson);
+			 //break;
+			if (height > 90 && height < 200) {
+				System.out.println("Allowed to Take Rides");
+			}
+
+			else {
+				System.out.println("Not Allowed");
+		
+			}
 		}
 
 	}
-
 }
