49771adda7601c7f60f13d1416247aaf639c6d93
unknown <kaurrajinder95@gmail.com>
2023-12-04 18:23:28 -0500

    assignment of descending order

---
+++ src/DescendingOrder/DescendingArr.java
@@ -0,0 +1,16 @@
+package DescendingOrder;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescendingArr {
+
+	Integer[] ar = { 15, 118, 35, 29, 174 };
+
+	void findDescNum() {
+
+		Arrays.sort(ar, Collections.reverseOrder());
+		
+
+	}
+}
---
+++ src/DescendingOrder/MainDescendingArr.java
@@ -0,0 +1,15 @@
+package DescendingOrder;
+
+import java.util.Arrays;
+
+public class MainDescendingArr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DescendingArr da=new DescendingArr();
+		da.findDescNum();
+		System.out.println("Modified Array in ascending Order is :" +Arrays.toString(da.ar));
+
+	}
+
+}
